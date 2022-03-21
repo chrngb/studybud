38cf68df6e6e69999468aede7a486460f9cb6349
chrngb <45155393+chrngb@users.noreply.github.com>
2022-03-21 09:04:06 +0545

    updated files

--- base/views.py
+++ base/views.py
@@ -1,5 +1,7 @@
 from multiprocessing import context
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+
+from .forms import RoomForm
 from .models import Room
 
 
@@ -21,3 +23,38 @@ def room(request, pk):
     context = {'room': room}
     return render(request, 'base/room.html', context)
 
+
+def createRoom(request):
+    if request.method == 'POST':
+        form = RoomForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+            
+    form = RoomForm()
+    context = {'form': form}
+    return render(request, 'base/room_form.html', context)
+
+def updateRoom(request, pk):
+    room = Room.objects.get(id=pk)
+    form = RoomForm(instance=room)
+
+    if request.method == 'POST':
+        form = RoomForm(request.POST, instance=room)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+
+    context = {'form': form}
+    return render(request, 'base/room_form.html', context)
+
+def deleteRoom(request, pk):
+    room = Room.objects.get(id=pk)
+    form = RoomForm(instance=room)
+
+    if request.method == 'POST':
+        room.delete()
+        return redirect('home')
+
+    # context = {'obj': room}
+    return render(request, 'base/delete.html', {'obj': room})
